533564c5875a97349e9a32aeff0c8f6b4ab5da6e
DavidJm <danimax.com@gmail.com>
2021-07-03 19:02:23 -0500

    Added navigator page - Fixed strings - Fixed navigation - Added new string languages

--- app/src/main/java/com/umb/activity2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/umb/activity2/ui/gallery/GalleryFragment.java
@@ -1,42 +1,59 @@
 package com.umb.activity2.ui.gallery;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.umb.activity2.R;
+import com.google.android.material.textfield.TextInputEditText;
 import com.umb.activity2.databinding.FragmentGalleryBinding;
+
+import java.util.Objects;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
+    private TextInputEditText userUrlText;
+    private Button btnSearchUrl;
+    private Editable urlWb;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
-
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        userUrlText = binding.wViewUserUmbUrl;
+        btnSearchUrl = binding.btnUrlWbSearch;
+        urlWb = Objects.requireNonNull(userUrlText.getText());
+        btnSearchUrl.setOnClickListener(v -> {
+            urlWb = userUrlText.getText();
+            setPageAndLoad(binding, urlWb);
+        } );
+
+        setPageAndLoad(binding, urlWb);
+
         return root;
     }
+
+    private void setPageAndLoad(FragmentGalleryBinding binding, Editable url)
+    {
+        final WebView view = binding.wViewUserUmb;
+        view.setWebViewClient(new WebViewClient());
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl("https://" + url);
+    }
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/umb/activity2/ui/home/HomeFragment.java
+++ app/src/main/java/com/umb/activity2/ui/home/HomeFragment.java
@@ -6,15 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.umb.activity2.R;
 import com.umb.activity2.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
